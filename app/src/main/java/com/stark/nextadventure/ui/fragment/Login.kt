25fc0dd9129daaaefdf3d3628ee6affb9d86b6f3
Gabriel Xavier <gabriel.xs.silva39@gmail.com>
2021-01-30 21:53:01 -0300

    :art: Create new Fragment

---
+++ app/src/main/java/com/stark/nextadventure/ui/fragment/Login.kt
@@ -0,0 +1,34 @@
+package com.stark.nextadventure.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.stark.nextadventure.R
+
+class Login : Fragment() {
+
+    private val navControl by lazy{
+        findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button).apply {
+            this.setOnClickListener {
+
+//                navControl.navigate()
+            }
+        }
+    }
+}
